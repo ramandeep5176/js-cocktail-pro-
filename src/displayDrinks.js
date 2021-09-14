a31dc31b915c2704b2272a106638e7215c7d7159
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-14 17:51:10 -0400

    search

---
+++ src/displayDrinks.js
@@ -0,0 +1,30 @@
+//
+import get from "./getElement.js";
+//
+9;
+const displayDrinks = ({ drinks }) => {
+  const section = get(".section-center");
+  const title = get(".title");
+  if (!drinks) {
+    //   hide loading
+    title.textContent = "Sorry, no drinks matched your search";
+    section.innerHTML = null;
+    return;
+  }
+  const newDrinks = drinks
+
+    .map((drink) => {
+      const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
+      return `  <a href="drink.html"
+          ><article class="cocktail" data-id="${id}">
+            <img src="${image}" alt='${name}' />
+            <h3>${name}</h3> </article></a>`;
+    })
+    .join(" ");
+  //hideloading
+  title.textContent = "";
+  section.innerHTML = newDrinks;
+  return section;
+};
+
+export default displayDrinks;
